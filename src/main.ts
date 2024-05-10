df32b3ac88bc8a92f22d8ee5b2d99328f9e92996
Zalven <zalven.dayao@go.team>
2024-05-11 01:17:14 +0800

    Working on extension

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.config.performance = true
+
 
 app.use(createPinia())
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+/// <reference types="chrome" />
+
 import { Button } from '@/components/ui/button'
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  let [tab] = await chrome.tabs.query({ active: true })
+  console.log(tab)
+})
 </script>
 
 
